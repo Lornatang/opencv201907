31abd6b31a7ad347b49e6542cb1c31de66c21a38
lornatang <shiyipaisizuo@163.com>
2019-08-15 15:36:00 +0800

    Update dlcv_ocr.cc

--- utils/dlcv_ocr.cc
+++ utils/dlcv_ocr.cc
@@ -19,6 +19,12 @@
 using namespace cv;
 using namespace std;
 
+Point2f center(0, 0);
+
+// finally image height and width
+int g_dst_height;
+int g_dst_width;
+
 /**
  * Convert color images to binary images for processing.
  * Args:
@@ -119,6 +125,274 @@ Mat deskew(const char *file_name, double angle) {
   Mat cropped;
   getRectSubPix(rotated, box_size, box.center, cropped);
   return cropped;
+}
+
+bool sort_corners(std::vector<cv::Point2f> &corners) {
+  std::vector<cv::Point2f> top, bot;
+  cv::Point2f tmp_pt;
+  std::vector<cv::Point2f> olddata = corners;
+
+  if (corners.size() != 4) {
+    return false;
+  }
+
+  for (size_t i = 0; i < corners.size(); i++) {
+    for (size_t j = i + 1; j < corners.size(); j++) {
+      if (corners[i].y < corners[j].y) {
+        tmp_pt = corners[i];
+        corners[i] = corners[j];
+        corners[j] = tmp_pt;
+      }
+    }
+  }
+  top.push_back(corners[0]);
+  top.push_back(corners[1]);
+  bot.push_back(corners[2]);
+  bot.push_back(corners[3]);
+  if (top.size() == 2 && bot.size() == 2) {
+    corners.clear();
+    cv::Point2f tl = top[0].x > top[1].x ? top[1] : top[0];
+    cv::Point2f tr = top[0].x > top[1].x ? top[0] : top[1];
+    cv::Point2f bl = bot[0].x > bot[1].x ? bot[1] : bot[0];
+    cv::Point2f br = bot[0].x > bot[1].x ? bot[0] : bot[1];
+    corners.push_back(tl);
+    corners.push_back(tr);
+    corners.push_back(br);
+    corners.push_back(bl);
+    return true;
+  } else {
+    corners = olddata;
+    return false;
+  }
+}
+
+cv::Point2f compute_intersect(cv::Vec4i a, cv::Vec4i b) {
+  int x1 = a[0], y1 = a[1], x2 = a[2], y2 = a[3];
+  int x3 = b[0], y3 = b[1], x4 = b[2], y4 = b[3];
+
+  if (float d = ((float) (x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4))) {
+    cv::Point2f pt;
+    pt.x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / d;
+    pt.y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / d;
+    return pt;
+  } else
+    return cv::Point2f(-1, -1);
+}
+
+bool IsBadLine(int a, int b) {
+  return a * a + b * b < 100;
+}
+
+bool x_sort(const Point2f &m1, const Point2f &m2) {
+  return m1.x < m2.x;
+}
+
+// Determine the center line of the four points
+void sort_corners(std::vector<cv::Point2f> &corners,
+                  cv::Point2f center) {
+  std::vector<cv::Point2f> top, bot;
+  vector<Point2f> backup = corners;
+  sort(corners.begin(), corners.end(), x_sort);
+
+  for (int i = 0; i < corners.size(); i++) {
+    // this is less than 2 to avoid the case where all three vertices are at top
+    if (corners[i].y < center.y && top.size() < 2)
+      top.push_back(corners[i]);
+    else
+      bot.push_back(corners[i]);
+  }
+  corners.clear();
+
+  if (top.size() == 2 && bot.size() == 2) {
+    cout << "log" << endl;
+    cv::Point2f tl = top[0].x > top[1].x ? top[1] : top[0];
+    cv::Point2f tr = top[0].x > top[1].x ? top[0] : top[1];
+    cv::Point2f bl = bot[0].x > bot[1].x ? bot[1] : bot[0];
+    cv::Point2f br = bot[0].x > bot[1].x ? bot[0] : bot[1];
+
+    corners.push_back(tl);
+    corners.push_back(tr);
+    corners.push_back(br);
+    corners.push_back(bl);
+  } else {
+    corners = backup;
+  }
+}
+
+void cal_dst_size(const vector<cv::Point2f> &corners) {
+  int h1 = sqrt((corners[0].x - corners[3].x) * (corners[0].x - corners[3].x) +
+                (corners[0].y - corners[3].y) * (corners[0].y - corners[3].y));
+  int h2 = sqrt((corners[1].x - corners[2].x) * (corners[1].x - corners[2].x) +
+                (corners[1].y - corners[2].y) * (corners[1].y - corners[2].y));
+  g_dst_height = MAX(h1, h2);
+
+  int w1 = sqrt((corners[0].x - corners[1].x) * (corners[0].x - corners[1].x) +
+                (corners[0].y - corners[1].y) * (corners[0].y - corners[1].y));
+  int w2 = sqrt((corners[2].x - corners[3].x) * (corners[2].x - corners[3].x) +
+                (corners[2].y - corners[3].y) * (corners[2].y - corners[3].y));
+  g_dst_width = MAX(w1, w2);
+}
+
+void adjust(const char *raw_path, const char *new_path) {
+  Mat src = imread(raw_path);
+  Mat source = src.clone();
+  Mat temp = src.clone();
+  Mat img = src.clone();
+  cvtColor(img, img, COLOR_RGB2GRAY);
+  GaussianBlur(img, img, Size(5, 5), 0, 0);
+
+  // Get the custom kernel
+  // The first parameter MORPH_RECT represents the convolution kernel of the rectangle.
+  // Of course, elliptic and intersected ones can also be selected
+  Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
+  // dilate ops. very good!
+  dilate(img, img, element);
+  // edge extraction
+  Canny(img, img, 30, 120, 3);
+
+  vector<vector<Point> > contours;
+  vector<vector<Point> > f_contours;
+  std::vector<cv::Point> approx2;
+  // Note that the fifth parameter, CV_RETR_EXTERNAL, retrieves only the outer box
+  findContours(img, f_contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+
+  // Find the outline with the largest area
+  int max_area = 0;
+  int index = 0;
+  for (int i = 0; i < f_contours.size(); i++) {
+    double tmparea = fabs(contourArea(f_contours[i]));
+    if (tmparea > max_area) {
+      index = i;
+      max_area = tmparea;
+    }
+
+  }
+  contours.push_back(f_contours[index]);
+
+  vector<Point> tmp = contours[0];
+
+  for (int line_type = 1; line_type <= 3; line_type++) {
+    Mat black = img.clone();
+    black.setTo(0);
+    // Pay attention to the thickness of the line, do not choose too thin
+    drawContours(black, contours, 0, Scalar(255), line_type);
+
+    std::vector<Vec4i> lines;
+    std::vector<cv::Point2f> corners;
+    std::vector<cv::Point2f> approx;
+
+    int para = 10;
+    int flag = 0;
+    int round = 0;
+    for (; para < 300; para++) {
+      lines.clear();
+      corners.clear();
+      approx.clear();
+      center = Point2f(0, 0);
+
+      cv::HoughLinesP(black, lines, 1, CV_PI / 180, para, 30, 10);
+
+      // Filter lines that are too close
+      std::set<int> ErasePt;
+      for (int i = 0; i < lines.size(); i++) {
+        for (int j = i + 1; j < lines.size(); j++) {
+          if (IsBadLine(abs(lines[i][0] - lines[j][0]), abs(lines[i][1] - lines[j][1])) &&
+              (IsBadLine(abs(lines[i][2] - lines[j][2]), abs(lines[i][3] - lines[j][3])))) {
+            // add the bad line to the collection
+            ErasePt.insert(j);
+          }
+        }
+      }
+
+      int Num = lines.size();
+      while (Num != 0) {
+        std::set<int>::iterator j = ErasePt.find(Num);
+        if (j != ErasePt.end()) {
+          lines.erase(lines.begin() + Num - 1);
+        }
+        Num--;
+      }
+      if (lines.size() != 4) {
+        continue;
+      }
+
+      // Calculate the intersection of the lines and save the portion within the image range
+      for (int i = 0; i < lines.size(); i++) {
+        for (int j = i + 1; j < lines.size(); j++) {
+          cv::Point2f pt = compute_intersect(lines[i], lines[j]);
+          // Make sure the intersection is within the range of the image
+          if (pt.x >= 0 && pt.y >= 0 && pt.x <= src.cols && pt.y <= src.rows)
+            corners.push_back(pt);
+        }
+      }
+      if (corners.size() != 4) {
+        continue;
+      }
+#if 1
+      bool IsGoodPoints = true;
+
+      // Make sure the distance between points is large enough to eliminate the error points
+      for (int i = 0; i < corners.size(); i++) {
+        for (int j = i + 1; j < corners.size(); j++) {
+          int distance = sqrt((corners[i].x - corners[j].x) * (corners[i].x - corners[j].x) +
+                              (corners[i].y - corners[j].y) * (corners[i].y - corners[j].y));
+          if (distance < 5) {
+            IsGoodPoints = false;
+          }
+        }
+      }
+
+      if (!IsGoodPoints) continue;
+#endif
+      cv::approxPolyDP(cv::Mat(corners), approx, cv::arcLength(cv::Mat(corners), true) * 0.02, true);
+
+      if (lines.size() == 4 && corners.size() == 4 && approx.size() == 4) {
+        flag = 1;
+        break;
+      }
+    }
+
+    // Get mass center
+    for (int i = 0; i < corners.size(); i++)
+      center += corners[i];
+    center *= (1. / corners.size());
+
+    if (flag) {
+      cv::circle(temp, corners[0], 3, CV_RGB(255, 0, 0), -1);
+      cv::circle(temp, corners[1], 3, CV_RGB(0, 255, 0), -1);
+      cv::circle(temp, corners[2], 3, CV_RGB(0, 0, 255), -1);
+      cv::circle(temp, corners[3], 3, CV_RGB(255, 255, 255), -1);
+      cv::circle(temp, center, 3, CV_RGB(255, 0, 255), -1);
+
+      sort_corners(corners, center);
+//      cout << "corners size" << corners.size() << endl;
+//      cout << "tl:" << corners[0] << endl;
+//      cout << "tr:" << corners[1] << endl;
+//      cout << "br:" << corners[2] << endl;
+//      cout << "bl:" << corners[3] << endl;
+
+      cal_dst_size(corners);
+
+      cv::Mat quad = cv::Mat::zeros(g_dst_height, g_dst_width, CV_8UC3);
+      std::vector<cv::Point2f> quad_pts;
+      quad_pts.push_back(cv::Point2f(0, 0));
+      quad_pts.push_back(cv::Point2f(quad.cols, 0));
+      quad_pts.push_back(cv::Point2f(quad.cols, quad.rows));
+
+      quad_pts.push_back(cv::Point2f(0, quad.rows));
+
+      cv::Mat transmtx = cv::getPerspectiveTransform(corners, quad_pts);
+      cv::warpPerspective(source, quad, transmtx, quad.size());
+
+      /*process for id card*/
+      Mat local, gray;
+      cvtColor(quad, gray, COLOR_RGB2GRAY);
+      int blockSize = 25;
+      int constValue = 10;
+      adaptiveThreshold(gray, local, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, blockSize, constValue);
+      imwrite(new_path, local);
+    }
+  }
 }
 
 /**
